3abae8eee7516d231087e0864fc9c63ff635b0ef
cherrima <changhee.ghang@gmail.com>
2026-04-06 18:00:16 +0900

    update

--- fastapi/03_advanced_app.py
+++ fastapi/03_advanced_app.py
@@ -14,4 +14,24 @@ class News(BaseModel) :
 def analyze_news(news:News) :
     return {"news":news.title}
 
+# /stocks/005930/history?day=60&market=kospi
+@app.get("/stocks/{ticker}/{service}")
+def get_stock_info(ticker:str, service:str, day:int, market:str) : 
+    
+    message = {}
+    message['종목명'] = ticker
+    message['서비스'] = service
+    message['일자'] = day
+    message['장구분'] = market
+    
+    # message = f"'종목명' : {ticker}, '서비스' : {service}, '일자' : {day}, '장구분' : {market}"
+    # return {
+    #     "종목명": ticker,
+    #     "서비스": service,
+    #     "일자": day,
+    #     "장구분": market
+    # }
+    
+    return  message
+
 # uvicorn 03_advanced_app:app --reload --port 8003
--- fastapi/main.py
+++ fastapi/main.py
@@ -35,3 +35,24 @@ def get_item_name(item_id: int) -> str :
         case _ :
             return "알수없슴"
         
+
+# /stocks/005930/history?day=60&market=kospi
+@app.get("/stocks/{ticker}/{service}")
+def get_stock_info(ticker:str, service:str, day:int, market:str) : 
+    
+    message = {}
+    message['종목명'] = ticker
+    message['서비스'] = service
+    message['일자'] = day
+    message['장구분'] = market
+    
+    # message = f"'종목명' : {ticker}, '서비스' : {service}, '일자' : {day}, '장구분' : {market}"
+    # return {
+    #     "종목명": ticker,
+    #     "서비스": service,
+    #     "일자": day,
+    #     "장구분": market
+    # }
+    
+    return  message
+        
